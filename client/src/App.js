2980ca00b1f2c4c9cf7e55fa2f7e245d886e5eab
Cody Askew <codyaskew9@gmail.com>
2023-01-07 21:18:45 -0700

    added basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 import { Card } from 'react-bootstrap';
 import cody from './images/cody.jpg'
 import leihzel from './images/leihzel.jpg'
-
+import flyer from './images/flyer.jpg'
 function App() {
   return (
     <>
-    <div className='teachers'>
+    <div className='top'>
+    <img className='flyer' src={flyer} alt='flyer information'></img>
+    <div className='info'>
+      <p>Classes will be held after school on Tuesdays from 3:00pm to 4:15pm for 1st and 2nd graders,
+        and Thursday from 3:00pm to 4:15pm for 3rd, 4th, and 5th grade at Altura Preparatory School.
+        Students are invited to bring an afternoon snack.</p>
+        <p> Cash, check, or money order payments will be accepted upon parent pickup on your child's first day of the program. Paypal or credit card payments can be made at the bottom of the page. 
+        Please be sure to include your child's name with your payment. If paying by check or money order, please make it out to Cody Askew. 
+      </p>
+
+    </div>
+    </div>
       <h2> Meet The Teachers!
       </h2>
+    <div className='teachers'>
         <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={leihzel} />
+      <Card.Img style={{width: '18rem'}} variant="top" src={leihzel} />
       <Card.Body>
         <Card.Title>Ms. Leihzel</Card.Title>
         <Card.Text>
@@ -20,7 +32,7 @@ function App() {
       </Card.Body>
     </Card>
         <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={cody} />
+      <Card.Img style = {{width: '18rem'}}variant="top" src={cody} />
       <Card.Body>
         <Card.Title>Cody Askew</Card.Title>
         <Card.Text>
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { ThemeProvider } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ThemeProvider
+  breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+  minBreakpoint="xxs"
+>
   <React.StrictMode>
-    <App />
+  <App />
   </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
