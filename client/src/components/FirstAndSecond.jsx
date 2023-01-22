3246cf0b3b801788a3ef9f62220c4e5d4a4ea6f3
Cody Askew <codyaskew9@gmail.com>
2023-01-21 23:16:44 -0700

    spelling

--- client/src/components/FirstAndSecond.jsx
+++ client/src/components/FirstAndSecond.jsx
@@ -8,13 +8,13 @@ const FirstAndSecond = () => {
       <h2>What We Are Working On</h2>
       </div>
       <div className='Work'>
-      <p> With our younger class our primary focus is classroom technology, and basic computer skills.
-        we are working on typing practice at the start of every class with <a href="https://www.typing.com/">Typing.com.</a>
+      <p> With our younger class, our primary focus is classroom technology and basic computer skills.
+        We are working on typing practice at the start of every class with <a href="https://www.typing.com/">Typing.com.</a>
         <br />
-      After we are warmed up and ready to type we do a guided lesson. This week we are working on learning sequencing learning how we have to be very clear with instructions.
-      This transfers to our next part where we work on coding animation with <a href="https://scratch.mit.edu/">Scratch.</a>
+      After we are warmed up and ready to type, we do a guided lesson. This week we are working on learning sequencing and learning how to be very clear with instructions.
+      This will transfer to the next lesson, where we work on coding animation with <a href="https://scratch.mit.edu/">Scratch.</a>
       <br />
-      At this time we do not have accounts set up for all of our student due to a internet outage right at the end of class. 
+      At this time we do not have accounts set up yet for all of our students due to an internet outage right at the end of class. 
       <br />
       <br />
       Pictures to come
--- client/src/components/ThirdToFifth.jsx
+++ client/src/components/ThirdToFifth.jsx
@@ -9,11 +9,11 @@ const ThirdToFifth = () => {
       </div>
       <div className='work'>
 
-      <p>With our third grade and up class we start our day with a typing warm-up with <a href='https://www.typing.com/'>Typing.com.</a>
-      <br /> From there we are off to our guided lesson on <a href='https://codesandbox.io/'>Code Sand Box</a>.<br /> We are starting off with learning HTML and the language of the internet as I believe it a invaluable skill for any coder.
-      I plan to spend a few weeks going over this and end with us making a website together. <br/> After the guided project they are free to work on coding animations or games with the help of <a href='https://scratch.mit.edu/'>Scratch</a>
+      <p>With our third trough fifth graders we start our day with a typing warm-up with <a href='https://www.typing.com/'>Typing.com.</a>
+      <br /> From there we are off to our guided lesson on <a href='https://codesandbox.io/'>Code Sand Box</a>.<br /> We are starting off with learning HTML and the language of the internet, as I believe it is an invaluable skill for any coder.
+      I plan to spend a few weeks going over this and end with a project of making a website as a group. <br/> After the guided project they are free to work on coding animations or games with the help of <a href='https://scratch.mit.edu/'>Scratch.</a>
       <br />
-      Due to a unforeseen internet issue we were unable to finish account setup this week but will have it working this week.
+      Due to an unforeseen internet issue we were unable to finish account setup this last week but will have it working this upcoming week.
       <br />
       <br/>
       Pictures to come!
